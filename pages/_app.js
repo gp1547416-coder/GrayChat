0c79fc49658501561bd78478ba35fcc6fe6b3a8c
gp1547416-coder <gp1547416@gmail.com>
2026-07-13 23:04:53 -0700

    Change import path for global styles to relative

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '@/styles/globals.css'
+// pages/_app.js
+import '../styles/globals.css'   // relative import – works without any config
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
